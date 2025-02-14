9dde9f228cf58256e2babec5e69b5832d36ddc34
Alejo <alejogustavof@gmail.com>
2025-02-14 20:27:15 -0300

    Refactor Mobile Grid

--- src/Components/Entity/Entity.tsx
+++ src/Components/Entity/Entity.tsx
@@ -10,34 +10,35 @@ function Entity({ data }: EntityProps) {
     return (
      <div className="m-3 text-white mx-auto max-w-full gap-x-4 bg-white p-6 shadow-lg outline outline-black/5">
         <div className="w-full mb-3 text-lg font-bold text-gray-900 "> {data.name}</div>
-        <div className="grid w-full grid-cols-4 gap-4">
-          <div className="flex flex-col">
+        <div className="grid w-full gap-4 grid-cols-1 sm:grid-cols-4">
+          <div className="flex flex-col sm:col-span-1">
             <label className="text-gray-400 text-xs">Email</label>
-            <span className="text-sky-400 text-ms font-medium">{data.email}</span> 
+            <span className="text-sky-400 text-ms font-medium">{data.email}</span>
           </div>
-          <div className="flex flex-col">
-            <label className="text-gray-400 text-xs">Invited</label>
-            <span className="text-gray-600 text-ms font-medium">{data.invited}</span> 
-          </div>
-          <div className="flex flex-col">
-            <label className="text-gray-400 text-xs">Role</label>
-            <span className="text-gray-600 text-ms font-medium">{data.role}</span> 
-          </div>
-          <div className="flex flex-col">
-            <label className="text-gray-400 text-xs">Status</label>
-            <span className="text-gray-600 text-ms font-medium">{data.status}</span> 
+          <div className="grid w-full gap-4 grid-cols-3 sm:col-span-3">
+            <div className="flex flex-col">
+              <label className="text-gray-400 text-xs">Invited</label>
+              <span className="text-gray-600 text-ms font-medium">{data.invited}</span>
+            </div>
+            <div className="flex flex-col">
+              <label className="text-gray-400 text-xs">Role</label>
+              <span className="text-gray-600 text-ms font-medium">{data.role}</span>
+            </div>
+            <div className="flex flex-col">
+              <label className="text-gray-400 text-xs">Status</label>
+              <span className="text-gray-600 text-ms font-medium">{data.status}</span>
+            </div>
           </div>
         </div>
-
-      <div className="w-full py-3 text-center mt-3">
-      {data.items?.map((item, index) => {
-        return (
-          <Permissions key={index} dataItem={item}  />
-        );
-      })}
-      </div>
+        <div className="w-full py-3 text-center mt-3">
+        {data.items?.map((item, index) => {
+          return (
+            <Permissions key={index} dataItem={item}  />
+          );
+        })}
+        </div>
     </div>
    );
-  }
+}
   
   export default Entity;
