1a41a82bd8fa444142b593bd6710a538c6384a5d
Alejo <alejogustavof@gmail.com>
2025-02-14 20:38:24 -0300

    Fix Identation

--- src/Components/Entity/Permissions.tsx
+++ src/Components/Entity/Permissions.tsx
@@ -7,36 +7,33 @@ interface EntityProps {
 
 function Permissions({ dataItem }: EntityProps) {
     return (
-        <>
-
-     <div className="grid w-full grid-cols-5 gap-4 dark:bg-gray-100 p-3 mt-3">
-        <label className="text-gray-400 text-xs flex items-center justify-center">{dataItem.title}</label>
-          <div className="flex flex-col">
-                <label className="text-gray-400 text-xs">Create</label>
-                <span  className="text-gray-600 text-ms font-medium">
-                    {dataItem.create ? "SI" : "NO"}
-                </span>
-          </div>
-          <div className="flex flex-col">
-            <label className="text-gray-400 text-xs">Edit</label>
-            <span className="text-gray-600 text-ms font-medium">
-                {dataItem.edit ? "SI" : "NO"}
-            </span> 
-          </div>
-          <div className="flex flex-col">
-            <label className="text-gray-400 text-xs">View</label>
-            <span className="text-gray-600 text-ms font-medium">
-                {dataItem.view ? "SI" : "NO"}
-            </span> 
-          </div>
-          <div className="flex flex-col">
-            <label className="text-gray-400 text-xs">Delete</label>
-            <span className="text-gray-600 text-ms font-medium">
-                {dataItem.delete ? "SI" : "NO"}
-            </span> 
-          </div>
+        <div className="grid w-full grid-cols-5 gap-4 dark:bg-gray-100 p-3 mt-3">
+            <label className="text-gray-400 text-xs flex items-center justify-center">{dataItem.title}</label>
+            <div className="flex flex-col">
+                    <label className="text-gray-400 text-xs">Create</label>
+                    <span  className="text-gray-600 text-ms font-medium">
+                        {dataItem.create ? "SI" : "NO"}
+                    </span>
+            </div>
+            <div className="flex flex-col">
+                <label className="text-gray-400 text-xs">Edit</label>
+                <span className="text-gray-600 text-ms font-medium">
+                    {dataItem.edit ? "SI" : "NO"}
+                </span> 
+            </div>
+            <div className="flex flex-col">
+                <label className="text-gray-400 text-xs">View</label>
+                <span className="text-gray-600 text-ms font-medium">
+                    {dataItem.view ? "SI" : "NO"}
+                </span> 
+            </div>
+            <div className="flex flex-col">
+                <label className="text-gray-400 text-xs">Delete</label>
+                <span className="text-gray-600 text-ms font-medium">
+                    {dataItem.delete ? "SI" : "NO"}
+                </span> 
+            </div>
         </div>
-     </>
    );
   }
   
