b6df36d7272b9dae2ab59b8d13cea57db5b354ca
Alejo <alejogustavof@gmail.com>
2025-02-15 18:16:50 -0300

    Add menu mobile and Logo

--- src/App.tsx
+++ src/App.tsx
@@ -28,12 +28,17 @@ function App() {
       <div className="flex h-screen">
         
         <aside
-        className={`absolute lg:relative inset-y-0 left-0 w-64 bg-blue-800 text-white transform transition-transform duration-300 z-40
+        className={`absolute lg:relative inset-y-0 left-0 w-full sm:w-64 bg-sky-950 text-white transform transition-transform duration-300 z-40
           ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0`}
           >
-          <div className="p-4">
+          <div className="p-4 flex justify-between">
             <h2 className="text-2xl font-bold">Menu</h2>
-            <button onClick={toggleSidebar} className='md:hidden'>CLOSE</button>
+            <button onClick={toggleSidebar} className='md:hidden'>
+              <img
+                  src= "/close.svg"
+                  alt="close"
+              />
+            </button>
           </div>
           <nav className="mt-4">
             <ul>
@@ -45,15 +50,21 @@ function App() {
           </nav>
         </aside>
         <div className="flex flex-col flex-1">
-          <header className="bg-white shadow p-4">
-            <h1 className="text-xl font-semibold">Dashboard</h1>
+          <header className="bg-white shadow p-4 flex justify-between">
+            <div className="flex items-center space-x-4">
+              <img
+                  src= "/logo.svg"
+                  alt="logo"
+                />
+              <h1 className="text-xl font-semibold">Dashboard</h1>
+            </div>
             <button   onClick={toggleSidebar} id="menu-toggle" className="block lg:hidden p-2 text-blue-800 border border-blue-800 rounded">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
               </svg>
             </button>
           </header>
-          <main className="flex-1 p-6">
+          <main className="flex-1 p-3 sm:p-6">
             <div id="react-wrapper">
               {entities?.map((entity, index) => {
                   return (
