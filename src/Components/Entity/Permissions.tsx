792e525659047c1462d747f996645559c63b8299
Alejo <alejogustavof@gmail.com>
2025-02-15 05:12:52 -0300

    Adjust font size on Mobile

--- src/Components/Entity/Permissions.tsx
+++ src/Components/Entity/Permissions.tsx
@@ -8,7 +8,7 @@ interface EntityProps {
 function Permissions({ dataItem }: EntityProps) {
     return (
         <div className="grid w-full grid-cols-5 gap-4 dark:bg-gray-100 p-3 mt-3">
-            <label className="text-gray-400 text-xs flex items-center justify-center">{dataItem.title}</label>
+            <label className="text-gray-500 flex items-center justify-center text-xs md:text-sm  font-bold">{dataItem.title}</label>
             <div className="flex flex-col items-center">
                     <label className="text-gray-400 text-xs">Create</label>
                     <span  className="text-gray-600 text-ms font-medium">
